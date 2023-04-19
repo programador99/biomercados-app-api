c0dc98682b52bbbd530b6a077b242e83202013c6
d34ckgler <herogamers.ortiz@gmail.com>
2023-04-19 10:00:47 -0400

    Ajuste de puerto

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-app.set('port',process.env.PORT|| 3001)
+app.set('port',process.env.PORT|| 3010)
 
 app.use(express.json());
 
